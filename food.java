35bcf0a65530d781efbd5b731fbcb73c21c27ff8
aakash002408-boop <aakash002408@gmail.com>
2026-01-29 18:54:47 +0530

    homework

---
+++ food.java
@@ -0,0 +1,19 @@
+class Food 
+{
+	public static void main(String[] args)
+	{
+		String foodname = "panneer butter masala";
+		String cusinetype = "India";
+		int calories = 400;
+		boolean isvegeterian = true;
+		double price = 250.50;
+		
+		System.out.println("foodname :" + foodname);
+		System.out.println("cusine Type :" + cusinetype);
+		System.out.println("calories :" + calories);
+		System.out.println("is vegeterian :" + isvegeterian);
+		System.out.println("price :" + price);
+		
+		 
+	}
+}
