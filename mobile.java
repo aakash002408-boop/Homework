66796308435fbadea913b51d8ced767b3df336fb
aakash002408-boop <aakash002408@gmail.com>
2026-01-29 17:46:24 +0530

    homework

---
+++ mobile.java
@@ -0,0 +1,20 @@
+class Mobile
+{
+	public static void main(String[] args)
+	{
+		String mobilename = "Galaxy 24 ultra";
+		String mobilebrand = "Samsung";
+		int batterycapacity = 5000;
+		float screensize = 5.8f;
+		double price = 74000.58;
+		
+		
+		System.out.println(mobilename);
+		System.out.println(mobilebrand);
+		System.out.println(batterycapacity);
+		System.out.println(screensize);
+		System.out.println(price);
+		
+	}
+	
+}
